e0e51b69f12b00641fb5dcd0176d6de0900c9fb2
aman <amanpachauria@gmail.com>
2024-01-06 19:30:11 +0530

    client signin complete

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,84 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function SignIn() {
+  const [formData, setFormData] = useState({
+    emailOrPhoneNumber: "", 
+    password: "",
+  });
+  
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    try {
+        setLoading(true);
+        const response = await fetch('/server/auth/signin', {
+             method: 'POST',
+             headers: {
+               'Content-Type': 'application/json',
+             },
+             body: JSON.stringify(formData),
+        });
+        const data = await response.json();
+        if( data.success === false ) {
+          setLoading(false);
+          setError(data.message);
+          return;
+        }
+        setLoading(false);
+        setError(null);
+        navigate('/');
+    } catch (error) {
+        setLoading(false);
+        setError(error.message);
+    }
+  };
+
   return (
-    <div>SignIn</div>
-  )
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <div className="bg-white p-8 rounded shadow-lg w-full max-w-md">
+        <h1 className="text-3xl font-semibold mb-6 text-center">Sign In</h1>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+          <input
+            className="border p-3 rounded-lg focus:outline-none focus:border-blue-500"
+            type="text"
+            name="emailOrPhoneNumber" 
+            placeholder="Email or Phone Number"
+            value={formData.emailOrPhoneNumber}
+            onChange={handleChange}
+            required
+          />
+
+          <input
+            className="border p-3 rounded-lg focus:outline-none focus:border-blue-500"
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+
+          <button
+            type="submit"
+            className="bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue"
+          >
+            Sign In
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
