7782af15f9eeaeec0820d33ddef4e3e73452f8bb
aman <amanpachauria@gmail.com>
2024-01-10 05:48:24 +0530

    fix bug

--- server/index.js
+++ server/index.js
@@ -2,42 +2,47 @@ import express from 'express';
 import mongoose from 'mongoose';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
-import listingRouter from './routes/listing.route.js';
+import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import listingRouter from './routes/listing.route.js'
+import path from 'path';
 
-dotenv.config(); 
+dotenv.config();
 
 mongoose.connect(process.env.MONGO)
-    .then(() => {
-        console.log('Connected to MongoDB!');
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+        .then( () => {
+            console.log('Connected to MongoDB!');
+        })
+        .catch( (err) => {
+            console.log(err);
+        });
 
 const app = express();
 app.use(cookieParser());
+
+const __dirname = path.resolve();
+
+app.listen(3000, ()=> {
+    console.log('Server is running on port 3000!');
+})
+
 app.use(express.json());
-
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client', 'dist'))); 
-}
-
 app.use('/server/user', userRouter);
 app.use('/server/auth', authRouter);
 app.use('/server/listing', listingRouter);
 
-// Global error handler middleware
-app.use((err, req, res, next) => {
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
+
+app.use( (err, req, res, next) => {
     const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
+    const message = err.message || "Internal Server Error";
     return res.status(statusCode).json({
         success: false,
         statusCode,
         message,
     });
 });
-
-// Export the app for Vercel to handle
-export default app;
